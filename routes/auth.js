69ca0e6da86c2a8f1a13a1b11a99f1594cb6d757
PRAVEEN2902 <praveenkumarpdkt25@gmail.com>
2020-08-02 22:08:08 +0530

    Admin commit

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,25 @@ router.get('/google',passport.authenticate('google',{
 }));
 
 router.get('/google/redirect',passport.authenticate('google'),(req,res)=>{
-    res.redirect('/home');
+  admin.find({email:req.user.email}).then(result=>{
+    if(result.length!=0)
+      {
+        var quans,verbals,logical;
+        model.logic.find({}).then(datal=>{
+          logical=datal
+          model.verbal.find({}).then(datav=>{
+            verbals=datav
+            model.quantitative.find({}).then(dataq=>{
+              quans=dataq
+              res.render('admin',{logical,verbals,quans})
+            })
+          })
+        })
+      }
+      else{
+        res.redirect('/home');
+      }
+  })
 })
 
 router.get('/logout',(req,res)=>{
